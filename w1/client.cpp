df5db1bcfd156820e86c4f1a491863e2c3fe1e37
MindamSer <androsovy.mail.ru@gmail.com>
2025-02-18 17:59:27 +0300

    server listens and replies, client sends and waits for reply

--- w1/client.cpp
+++ w1/client.cpp
@@ -1,33 +1,64 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/select.h>
+
 #include <netdb.h>
-#include <cstring>
-#include <cstdio>
-#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <arpa/inet.h>
+
 #include "socket_tools.h"
 
 int main(int argc, const char **argv)
 {
-  const char *port = "2025";
+  const char *serverAdress = "localhost";
+  const char *serverPort = "2025";
+  addrinfo serverAddrInfo;
 
-  addrinfo resAddrInfo;
-  int sfd = create_dgram_socket("localhost", port, &resAddrInfo);
+  int sfd = create_dgram_socket(serverAdress, serverPort, &serverAddrInfo);
 
   if (sfd == -1)
   {
-    printf("Cannot create a socket\n");
+    printf("Cannot create a client socket!");
     return 1;
   }
 
+
+  char *buffer = new char[BUF_SIZE];
+  ssize_t sendRes = -1; 
+  ssize_t recvRes = -1; 
+
   while (true)
   {
-    std::string input;
     printf(">");
-    std::getline(std::cin, input);
-    ssize_t res = sendto(sfd, input.c_str(), input.size(), 0, resAddrInfo.ai_addr, resAddrInfo.ai_addrlen);
-    if (res == -1)
-      std::cout << strerror(errno) << std::endl;
+    scanf("%s", buffer);
+    
+    sendRes = sendto(
+      sfd, buffer, BUF_SIZE,
+      MSG_WAITALL, serverAddrInfo.ai_addr, serverAddrInfo.ai_addrlen);
+    if (sendRes == -1)
+    { 
+      printf("%s", strerror(errno)); 
+    }
+
+    while(recvRes < 0)
+    {
+      recvRes = recvfrom(
+        sfd, buffer, BUF_SIZE,
+        MSG_WAITALL, serverAddrInfo.ai_addr, &serverAddrInfo.ai_addrlen);
+      if (recvRes > 0)
+      {
+        printf("serv_resp: %s\n", buffer);
+      }
+    }
+
+    sendRes = -1;
+    recvRes = -1;
   }
+
+
+  delete[] buffer;
+
   return 0;
 }
