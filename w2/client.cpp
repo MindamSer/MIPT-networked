5406694f14c5f59de932806613cbe83cd04419d9
MindamSer <androsovy.mail.ru@gmail.com>
2025-03-04 16:27:09 +0300

    cleaned code a little

--- w2/client.cpp
+++ w2/client.cpp
@@ -3,10 +3,13 @@
 #include <cstring>
 #include <iostream>
 
+#include"settings.h"
+
+
 void send_fragmented_packet(ENetPeer *peer)
 {
   const char *baseMsg = "Stay awhile and listen. ";
-  const size_t msgLen = std::strlen(baseMsg);
+  const size_t msgLen = strlen(baseMsg);
 
   const size_t sendSize = 2500;
   char *hugeMessage = new char[sendSize];
@@ -29,53 +32,66 @@ void send_micro_packet(ENetPeer *peer)
 
 int main(int argc, const char **argv)
 {
-  int width = 800;
-  int height = 600;
-  InitWindow(width, height, "w2 MIPT networked");
-
-  const int scrWidth = GetMonitorWidth(0);
-  const int scrHeight = GetMonitorHeight(0);
-  if (scrWidth < width || scrHeight < height)
+  int width = WIN_W;
+  int height = WIN_H;
   {
-    width = std::min(scrWidth, width);
-    height = std::min(scrHeight - 150, height);
-    SetWindowSize(width, height);
-  }
+    InitWindow(width, height, WIN_TITLE);
 
-  SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    const int scrWidth = GetMonitorWidth(0);
+    const int scrHeight = GetMonitorHeight(0);
+    if (scrWidth < width || scrHeight < height)
+    {
+      width = std::min(scrWidth - WIN_BORD, width);
+      height = std::min(scrHeight - WIN_BORD, height);
+      SetWindowSize(width, height);
+    }
+
+    SetTargetFPS(APP_FPS);
+  }
+  std::cout << "Window initialized" << std::endl;
+
 
   if (enet_initialize() != 0)
   {
-    printf("Cannot init ENet");
+    std::cout << "Cannot init ENet" << std::endl;
     return 1;
   }
+  atexit(enet_deinitialize);
+  std::cout << "ENet initialized" << std::endl;
+
 
   ENetHost *client = enet_host_create(nullptr, 1, 2, 0, 0);
   if (!client)
   {
-    printf("Cannot create ENet client\n");
+    std::cout << "Cannot create ENet client" << std::endl;
     return 1;
   }
+  std::cout << "ENet client created" << std::endl;
+
 
   ENetAddress address;
-  enet_address_set_host(&address, "localhost");
-  address.port = 10887;
+  ENetPeer *lobbyPeer;
 
-  ENetPeer *lobbyPeer = enet_host_connect(client, &address, 2, 0);
-  if (!lobbyPeer)
+  enet_address_set_host(&address, LOBBY_ADDRESS);
+  address.port = LOBBY_PORT;
+  if (!(lobbyPeer = enet_host_connect(client, &address, 2, 0)))
   {
-    printf("Cannot connect to lobby");
+    std::cout << "Cannot connect to lobby" << std::endl;
     return 1;
   }
+  std::cout << "Connected to lobby" << std::endl;
+
 
   uint32_t timeStart = enet_time_get();
   uint32_t lastFragmentedSendTime = timeStart;
   uint32_t lastMicroSendTime = timeStart;
   bool connected = false;
-  float posx = GetRandomValue(100, 1000);
-  float posy = GetRandomValue(100, 500);
+  float posx = GetRandomValue(50, width - 50);
+  float posy = GetRandomValue(50, height - 50);
   float velx = 0.f;
   float vely = 0.f;
+
+
   while (!WindowShouldClose())
   {
     const float dt = GetFrameTime();
@@ -96,7 +112,7 @@ int main(int argc, const char **argv)
         break;
       };
     }
-    if (connected)
+    if (false)
     {
       uint32_t curTime = enet_time_get();
       if (curTime - lastFragmentedSendTime > 1000)
@@ -114,7 +130,7 @@ int main(int argc, const char **argv)
     bool right = IsKeyDown(KEY_RIGHT);
     bool up = IsKeyDown(KEY_UP);
     bool down = IsKeyDown(KEY_DOWN);
-    constexpr float accel = 30.f;
+    constexpr float accel = 150.f;
     velx += ((left ? -1.f : 0.f) + (right ? 1.f : 0.f)) * dt * accel;
     vely += ((up ? -1.f : 0.f) + (down ? 1.f : 0.f)) * dt * accel;
     posx += velx * dt;
@@ -130,5 +146,7 @@ int main(int argc, const char **argv)
       DrawCircleV(Vector2{posx, posy}, 10.f, WHITE);
     EndDrawing();
   }
+
+
   return 0;
 }
--- w2/lobby.cpp
+++ w2/lobby.cpp
@@ -1,49 +1,66 @@
+#include <cstdlib>
 #include <enet/enet.h>
 #include <iostream>
+
+#include"settings.h"
+
 
 int main(int argc, const char **argv)
 {
   if (enet_initialize() != 0)
   {
-    printf("Cannot init ENet");
+    std::cout << "Cannot init ENet" << std::endl;
     return 1;
   }
+  atexit(enet_deinitialize);
+  std::cout << "ENet initialized" << std::endl;
+
+
   ENetAddress address;
+  ENetHost *lobby;
 
   address.host = ENET_HOST_ANY;
-  address.port = 10887;
-
-  ENetHost *server = enet_host_create(&address, 32, 2, 0, 0);
-
-  if (!server)
+  address.port = LOBBY_PORT;
+  if (!(lobby = enet_host_create(&address, 32, 2, 0, 0)))
   {
-    printf("Cannot create ENet server\n");
+    std::cout << "Cannot create lobby" << std::endl;
     return 1;
   }
+  std::cout << "ENet host created" << std::endl;
+  
+
+  std::cout << "Lobby is active!" << std::endl;
+
 
   while (true)
   {
     ENetEvent event;
-    while (enet_host_service(server, &event, 10) > 0)
+    while (enet_host_service(lobby, &event, 10) > 0)
     {
       switch (event.type)
       {
       case ENET_EVENT_TYPE_CONNECT:
-        printf("Connection with %x:%u established\n", event.peer->address.host, event.peer->address.port);
+        printf("%x:%u - connecion established\n", event.peer->address.host, event.peer->address.port);
         break;
+
+      case ENET_EVENT_TYPE_DISCONNECT:
+        printf("%x:%u - disconnected\n", event.peer->address.host, event.peer->address.port);
+        break;
+
       case ENET_EVENT_TYPE_RECEIVE:
-        printf("Packet received '%s'\n", event.packet->data);
+        printf("Packet received:\n'%s'\n", event.packet->data);
         enet_packet_destroy(event.packet);
         break;
+
       default:
         break;
       };
     }
   }
 
-  enet_host_destroy(server);
 
-  atexit(enet_deinitialize);
+  enet_host_destroy(lobby);
+
   return 0;
 }
 
